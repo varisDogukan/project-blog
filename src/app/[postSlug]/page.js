629cf1d689e37ce25986438465c88115b4dbc032
dogukanVaris <varisdogukan@outlook.com>
2024-10-20 13:14:20 +0300

    Code snipptests with Bright

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -6,6 +6,7 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { BLOG_TITLE } from "@/constants";
+import CodeSnippet from "@/components/CodeSnippet";
 
 export async function generateMetadata({ params }) {
   const { frontmatter } = await loadBlogPost(params.postSlug);
@@ -26,7 +27,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
